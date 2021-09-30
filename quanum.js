9c26a3486bf156b6bc3a015ec25a1110873e77d8
y.b.l** <69856522+yblui@users.noreply.github.com>
2021-09-30 21:34:21 +0800

    2m

--- quanum.js
+++ quanum.js
@@ -1,22 +1,20 @@
 function $plus(pa, pb) {
     if (pa.indexOf(".") == -1) pa = pa + ".";
     if (pb.indexOf(".") == -1) pb = pb + ".";
-    pa = pa.split("");
-    pb = pb.split("");
-    if (pa.join("").split(".")[0].length > pb.join("").split(".")[0].length) {
-        for (var i = 0; i < (pa.join("").split(".")[0].length - pb.join("").split(".")[0].length); i++) {
-            pb.unshift("0");
-        }
+    var ppa = pa.split("");
+    var ppb = pb.split("");
+    if (ppa.join("").split(".")[0].length > ppb.join("").split(".")[0].length) {
+        var x = ppa.join("").split(".")[0].length - ppb.join("").split(".")[0].length;
+        for (var i = 0; i < x; i++) ppb.unshift("0");
     } else {
-        for (i = 0; i < (pb.join("").split(".")[0] - pa.join("").split(".")[0]); i++) {
-            pa.unshift("0");
-        }
+        x = ppb.join("").split(".")[0].length - ppa.join("").split(".")[0].length;
+        for (i = 0; i < x; i++) ppa.unshift("0");
     }
     var pc = [];
-    for (i = 0; i < Math.max(pa.length, pb.length); i++) {
-        if (pa[i] && pb[i] && pa[i] != ".") pc[i] = Number(pa[i]) + Number(pb[i])
-        else if (pa[i] == ".") pc[i] = "."
-        else pc[i] = Number(pa[i]) || Number(pb[i]);
+    for (i = 0; i < Math.max(ppa.length, ppb.length); i++) {
+        if (ppa[i] && ppb[i] && ppa[i] != ".") pc[i] = Number(ppa[i]) + Number(ppb[i]);
+        else if (ppa[i] == ".") pc[i] = ".";
+        else pc[i] = Number(ppa[i]) || Number(ppb[i]);
     }
     while (!pc.every($check)) {
         for (i = 0; i < pc.length; i++) {
@@ -26,7 +24,7 @@ function $plus(pa, pb) {
             } else if (pc[i] >= 10 && pc[i - 1] != ".") {
                 pc[i] -= 10;
                 pc[i - 1] += 1;
-            } else if(pc[i] >= 10 && pc[i - 1] == ".") {
+            } else if (pc[i] >= 10 && pc[i - 1] == ".") {
                 pc[i] -= 10;
                 pc[i - 2] += 1;
             }
@@ -37,4 +35,22 @@ function $plus(pa, pb) {
 
 function $check(val) {
     return val < 10 || val == ".";
+}
+
+function $times(ta, tb) {
+    var tta = ta.replace(/[\-\.]/g, "");
+    var ttb = tb.replace(/[\-\.]/g, "");
+    tta = tta.split("").reverse();
+    ttb = ttb.split("").reverse();
+    var ret = "0";
+    for (var i = 0; i < tta.length; i++) {
+        for (var j = 0; j < ttb.length; j++) {
+            var a = "";
+            for (var k = 0; k < i + j; k++) {
+                a = a + "0";
+            }
+            ret = $plus(ret, Number(tta[i]) * Number(ttb[j]) + a);
+        }
+    }
+    return ret;
 }
